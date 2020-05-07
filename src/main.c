d77f01990845321c20fe10a6492ced68d8cd3afb
Salvatore Raccardi <36445663+salvatoreraccardi@users.noreply.github.com>
2020-05-07 18:10:14 +0200

    v0.0.1

---
+++ src/main.c
@@ -0,0 +1,12 @@
+//
+//  v0.0.1
+//  Solitario - Reglan chiuso
+//  Raccardi Salvatore - Graziano Antonino
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}

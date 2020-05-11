1094e0bd4ce75bc70908e1d29f03fbd299dd0b45
Salvatore Raccardi <36445663+salvatoreraccardi@users.noreply.github.com>
2020-05-11 10:29:12 +0200

    keyboard input

--- src/main.c
+++ src/main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+// For keyboard input
+#include <conio.h>
+
+// Playground 
+int playground() {
+    printf("playground");
+    // ASCII CHAR - OK!
+    int i = 179;
+    printf("%c\n", i);
+    // ISSUES - UNICODE 
+    //wchar_t data = "\x2660\x2663\x2665\x2666";
+    printf("\2660");
+} 
+
+// Keyboard input
+int keyboard_interrupt(){
+    int value;
+    value = getch();
+    // printf("Keyboard request  -> %d\n", value);
+    return value;
+}
 
 int main() {
-    printf("Master");
+
+
+    while(1){
+        // KEYBOARD
+        if(keyboard_interrupt() == 27){
+            printf("ESC");
+            break; 
+        }else if(keyboard_interrupt() == 72){
+            printf("ARROW UP\n");
+        }else if(keyboard_interrupt() == 80){
+            printf("ARROW DOWN\n");
+        }else if(keyboard_interrupt() == 77){
+            printf("ARROW RIGHT\n");
+        }else if(keyboard_interrupt() == 75){
+            printf("ARROW LEFT\n");
+        }
+    }
+
     return 0;
 }

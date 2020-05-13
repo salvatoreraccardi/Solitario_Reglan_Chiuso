84462fe21649e5a5e45c86e0ad73f5ada601e94a
Salvatore Raccardi <36445663+salvatoreraccardi@users.noreply.github.com>
2020-05-13 09:34:47 +0200

    clear code

--- src/sector/sectorA.c
+++ src/sector/sectorA.c
@@ -1,104 +1,136 @@
 // K 13
 // Q 12
 // J 11
+
+// Vettori per la simulazione
 int array_1_valore[6] = {13, 12, 4, 10, 11, 5};
 char array_1_seme[6] = {'C', 'Q', 'C', 'Q', 'F', 'F'};
 char array_1_colore[6] = {'B', 'R', 'R', 'B', 'B', 'R'};
 
 void sector_A(int nCard, int *cardValore){
-    //int nCard = 6;
     int index = 0;
-    int step1 = 0;
-    int step2 = 0;
+    // offset1/2 per gestire la visualizzazione delle carte
+    int offset1 = 0;
+    int offset2 = 0;
 
     // TopCard
     for(index = 0; index < nCard; index++){   
+        // colore stampa
         if(array_1_colore[index] == 'R'){
             textcolor(12);
         }else{
             textcolor(15);
         }
-        gotoxy(step1, 0);  
+        // imposto la posizione del cursore (x,y)
+        gotoxy(offset1, 0);
+        // print ┌
         printf("%c", 218);
         for(int i = 0; i < 5; i++)
+            // print ─
             printf("%c", 196);
-        printf("%c", 191);       
-        step1 += 8;
+        // print ┐    
+        printf("%c", 191);      
+        // traslazione di 8pixel per il print successivo 
+        offset1 += 8;
+        // OUTPUT_1: ┌─────┐
     }
     
     //MidCard - valore
-    step1 = 0;
-    step2 = 6;
+    offset1 = 0;
+    offset2 = 6;
     for(int index = 0; index < nCard; index++){
+        // colore stampa
         if(array_1_colore[index] == 'R'){
             textcolor(12);
         }else{
             textcolor(15);
         }
-        gotoxy(step1, 1);
+        gotoxy(offset1, 1);
+        // print valore carta - (1...K)
         printf("%c %i", 179, *(cardValore + index));
         
-        gotoxy(step2, 1);
+        gotoxy(offset2, 1);
         printf("%c", 179);
 
-        step1 += 8;
-        step2 += 8;
+        // traslazione di 8pixel per il print successivo 
+        offset1 += 8;
+        offset2 += 8;
+        // OUTPUT_2: │ 8   │
     }
 
     //MidCard - empty
-    step1 = 0;
-    step2 = 6;
+    offset1 = 0;
+    offset2 = 6;
     for(index = 0; index < nCard; index++){
+        // colore stampa
         if(array_1_colore[index] == 'R'){
             textcolor(12);
         }else{
             textcolor(15);
         }
-        gotoxy(step1, 2);
+        gotoxy(offset1, 2);
         printf("%c", 179);
 
-        gotoxy(step2, 2);
+        gotoxy(offset2, 2);
         printf("%c", 179);
-        step1 += 8;
-        step2 += 8;
+        // traslazione di 8pixel per il print successivo 
+        offset1 += 8;
+        offset2 += 8;
+        // OUTPUT_3: │     │
     }      
 
     //MidCard - seme  
-    step1 = 0;
-    step2 = 6;
+    offset1 = 0;
+    offset2 = 6;
     for(index = 0; index < nCard; index++){
+        // colore stampa
         if(array_1_colore[index] == 'R'){
             textcolor(12);
         }else{
             textcolor(15);
         }
-        gotoxy(step1, 3);  
+        gotoxy(offset1, 3);  
         printf("%c", 179);
-        gotoxy(step1 + 5, 3);
+        gotoxy(offset1 + 5, 3);
+        // Print del seme
         printf("%c", array_1_seme[index]);
-        gotoxy(step2, 3);  
+        gotoxy(offset2, 3);  
         printf("%c", 179);
-        step1 += 8;
-        step2 += 8;
+        
+        // traslazione di 8pixel per il print successivo 
+        offset1 += 8;
+        offset2 += 8;
+        // OUTPUT_4: │    ♠│ 
     }
 
     //BottomCard - end
-    step1 = 0;
-    step2 = 6;
+    offset1 = 0;
+    offset2 = 6;
     for(index = 0; index < nCard; index++){
+        // colore stampa
         if(array_1_colore[index] == 'R'){
             textcolor(12);
         }else{
             textcolor(15);
         }
-        gotoxy(step1, 4);
+        gotoxy(offset1, 4);
         printf("%c", 192);
         for(int j = 0; j < 5; j++)
             printf("%c", 196);
-        gotoxy(step2, 4);    
+        gotoxy(offset2, 4);    
         printf("%c", 217);
-        step1 += 8;
-        step2 += 8;
+        // traslazione di 8pixel per il print successivo 
+        offset1 += 8;
+        offset2 += 8;
+        // OUTPUT_5: └─────┘
     }
 
+    // FINAL RESULT
+    // INDEX:      (0)     (1)     (2)     (3)
+    // COLOR:      (R)     (R)    (B/W)   (B/W)
+    // OUTPUT_1: ┌─────┐ ┌─────┐ ┌─────┐ ┌─────┐
+    // OUTPUT_2: │ 8   │ │ A   │ │ Q   │ │ 2   │
+    // OUTPUT_3: │     │ │     │ │     │ │     │
+    // OUTPUT_4: │    ♠│ │    ♥│ │    ♦│ │    ♣│
+    // OUTPUT_5: └─────┘ └─────┘ └─────┘ └─────┘
 }

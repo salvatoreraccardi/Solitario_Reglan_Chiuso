8405231dee50ee76e97426120e17d5365181cb6e
Salvatore Raccardi <36445663+salvatoreraccardi@users.noreply.github.com>
2020-05-17 16:51:15 +0200

    update

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include <windows.h>
 #include <time.h> 
 
-#include "services.c"
+#include "tools.c"
 #include "startup.c"
 #include "deck.c"
 #include "graphics/sectorA.c"
@@ -48,9 +48,9 @@ int main() {
 
     // puntatori <--data-- vettori
     for(i = 0; i < cardN; i++){
-        *(cardValore + i) = deckValore[45 + i];
-        *(cardColore + i) = deckColore[45 + i];
-        *(cardSeme + i) = deckSeme[45 + i];
+        *(cardValore + i) = deckValore[46 + i];
+        *(cardColore + i) = deckColore[46 + i];
+        *(cardSeme + i) = deckSeme[46 + i];
     }
 
     // generazione delle carte
@@ -73,10 +73,10 @@ int main() {
     sector_B(cardValore, cardColore, cardSeme);
 
    
-    int nCard = 6;
+    int nCard = 9;
     int offsetX = 0;
     // array_dataset
-    int arr_dorso[6] = {1, 1, 0, 0, 0, 0};
+    int arr_dorso[9] = {1, 1, 1, 1, 1, 1, 1, 1, 0};
     
     int *cDorso = NULL;
     int *cValore = NULL;
@@ -89,17 +89,24 @@ int main() {
     cSeme = (char*) calloc(nCard, sizeof(char));
     cColore = (char*) calloc(nCard, sizeof(char));
 
-    // puntatori <--data-- vettori
-    for(i = 0; i < nCard; i++){
-        *(cDorso + i) = arr_dorso[i];
-        *(cValore + i) = deckValore[4 + i];
-        *(cSeme + i) = deckSeme[4 + i];
-        *(cColore + i) = deckColore[4 + i];
+    int getCard[9] = {7, 7, 7, 6, 5, 4, 3, 2, 1};
+    int offsetData = 4;
+
+    for(int j = 0; j < 9; j++){
+        // puntatori <--data-- vettori
+        for(int i = 0; i < getCard[j]; i++){
+            *(cDorso + i) = arr_dorso[i];
+            *(cValore + i) = deckValore[offsetData + i];
+            *(cSeme + i) = deckSeme[offsetData + i];
+            *(cColore + i) = deckColore[offsetData + i];
+        }
+
+        // int nCard, bool array_dorso[3], int array_valore[3], char array_seme[3], char array_colore[3], int offsetX
+        sector_C(getCard[j], cDorso, cValore, cSeme, cColore, offsetX);
+        offsetData += getCard[j];
+        offsetX += 8;
     }
 
-    // int nCard, bool array_dorso[3], int array_valore[3], char array_seme[3], char array_colore[3], int offsetX
-    sector_C(nCard, cDorso, cValore, cSeme, cColore, offsetX);
-    
     //sector_C(5, 2, 8);
     //sector_C(6, 3, 16);
     //sector_C(numeroDorsi, numeroCard, 24);

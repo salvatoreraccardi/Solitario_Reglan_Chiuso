970e7978d2556a9e5689a1a9bc92721136be9182
Salvatore Raccardi <36445663+salvatoreraccardi@users.noreply.github.com>
2020-05-21 14:33:49 +0200

    update not ready for use

--- src/graphics/sectorA.c
+++ src/graphics/sectorA.c
@@ -4,63 +4,32 @@ void sector_A(int nCard, int *cardValore, char *cardColore, char *cardSeme){
     int offset1 = 0;
     int offset2 = 0;
 
-    // TopCard
-    for(index = 0; index < nCard; index++){   
-        // colore stampa
-        if(*(cardColore + index) == 'R'){
-            textcolor(12);
-        }else{
-            textcolor(15);
+    if((*cardValore + index) != 0){
+        // TopCard
+        for(index = 0; index < nCard; index++){   
+            // colore stampa
+            if(*(cardColore + index) == 'R'){
+                textcolor(12);
+            }else{
+                textcolor(15);
+            }
+            // imposto la posizione del cursore (x,y)
+            gotoxy(offset1, 0);
+            // print ┌
+            printf("%c", 218);
+            for(int i = 0; i < 5; i++)
+                // print ─
+                printf("%c", 196);
+            // print ┐    
+            printf("%c", 191);      
+            // traslazione di 8pixel per il print successivo 
+            offset1 += 8;
+            // OUTPUT_1: ┌─────┐
         }
-        // imposto la posizione del cursore (x,y)
-        gotoxy(offset1, 0);
-        // print ┌
-        printf("%c", 218);
-        for(int i = 0; i < 5; i++)
-            // print ─
-            printf("%c", 196);
-        // print ┐    
-        printf("%c", 191);      
-        // traslazione di 8pixel per il print successivo 
-        offset1 += 8;
-        // OUTPUT_1: ┌─────┐
-    }
-    
-    //MidCard - valore
-    offset1 = 0;
-    offset2 = 6;
-    for(index = 0; index < nCard; index++){
-        // colore stampa
-        if(*(cardColore + index) == 'R'){
-            textcolor(12);
-        }else{
-            textcolor(15);
-        }
-        gotoxy(offset1, 1);
-        // print valore carta - (2...K)
-        if(*(cardValore + index) <= 10){
-            printf("%c %i", 179, *(cardValore + index));
-        }else if(*(cardValore + index) == 11){
-            printf("%c J", 179);
-        }else if(*(cardValore + index) == 12){    
-            printf("%c Q", 179);
-        }else if(*(cardValore + index) == 13){
-            printf("%c K", 179);
-        }    
         
-        gotoxy(offset2, 1);
-        printf("%c", 179);
-
-        // traslazione di 8pixel per il print successivo 
-        offset1 += 8;
-        offset2 += 8;
-        // OUTPUT_2: │ 8   │
-    }
-
-    //MidCard - empty
-    offset1 = 0;
-    offset2 = 6;
-    for(int i = 0; i < 2; i++){
+        //MidCard - valore
+        offset1 = 0;
+        offset2 = 6;
         for(index = 0; index < nCard; index++){
             // colore stampa
             if(*(cardColore + index) == 'R'){
@@ -68,66 +37,98 @@ void sector_A(int nCard, int *cardValore, char *cardColore, char *cardSeme){
             }else{
                 textcolor(15);
             }
-            gotoxy(offset1, 2 + i);
+            gotoxy(offset1, 1);
+            // print valore carta - (2...K)
+            if(*(cardValore + index) <= 10){
+                printf("%c %i", 179, *(cardValore + index));
+            }else if(*(cardValore + index) == 11){
+                printf("%c J", 179);
+            }else if(*(cardValore + index) == 12){    
+                printf("%c Q", 179);
+            }else if(*(cardValore + index) == 13){
+                printf("%c K", 179);
+            }    
+            
+            gotoxy(offset2, 1);
             printf("%c", 179);
 
-            gotoxy(offset2, 2 + i);
-            printf("%c", 179);
             // traslazione di 8pixel per il print successivo 
             offset1 += 8;
             offset2 += 8;
-            // OUTPUT_3: │     │
-        }  
+            // OUTPUT_2: │ 8   │
+        }
+
+        //MidCard - empty
         offset1 = 0;
-        offset2 = 6;    
-    }
+        offset2 = 6;
+        for(int i = 0; i < 2; i++){
+            for(index = 0; index < nCard; index++){
+                // colore stampa
+                if(*(cardColore + index) == 'R'){
+                    textcolor(12);
+                }else{
+                    textcolor(15);
+                }
+                gotoxy(offset1, 2 + i);
+                printf("%c", 179);
 
-    //MidCard - seme  
-    offset1 = 0;
-    offset2 = 6;
-    for(index = 0; index < nCard; index++){
-        // colore stampa
-        if(*(cardColore + index) == 'R'){
-            textcolor(12);
-        }else{
-            textcolor(15);
+                gotoxy(offset2, 2 + i);
+                printf("%c", 179);
+                // traslazione di 8pixel per il print successivo 
+                offset1 += 8;
+                offset2 += 8;
+                // OUTPUT_3: │     │
+            }  
+            offset1 = 0;
+            offset2 = 6;    
         }
-        gotoxy(offset1, 4);  
-        printf("%c", 179);
-        gotoxy(offset1 + 5, 4);
-        // Print del seme
-        printf("%c", *(cardSeme + index));
-        gotoxy(offset2, 4);  
-        printf("%c", 179);
-        
-        // traslazione di 8pixel per il print successivo 
-        offset1 += 8;
-        offset2 += 8;
-        // OUTPUT_4: │    ♠│ 
-    }
 
-    //BottomCard - end
-    offset1 = 0;
-    offset2 = 6;
-    for(index = 0; index < nCard; index++){
-        // colore stampa
-        if(*(cardColore + index) == 'R'){
-            textcolor(12);
-        }else{
-            textcolor(15);
+        //MidCard - seme  
+        offset1 = 0;
+        offset2 = 6;
+        for(index = 0; index < nCard; index++){
+            // colore stampa
+            if(*(cardColore + index) == 'R'){
+                textcolor(12);
+            }else{
+                textcolor(15);
+            }
+            gotoxy(offset1, 4);  
+            printf("%c", 179);
+            gotoxy(offset1 + 5, 4);
+            // Print del seme
+            printf("%c", *(cardSeme + index));
+            gotoxy(offset2, 4);  
+            printf("%c", 179);
+            
+            // traslazione di 8pixel per il print successivo 
+            offset1 += 8;
+            offset2 += 8;
+            // OUTPUT_4: │    ♠│ 
         }
-        gotoxy(offset1, 5);
-        printf("%c", 192);
-        for(int j = 0; j < 5; j++)
-            printf("%c", 196);
-        gotoxy(offset2, 5);    
-        printf("%c", 217);
-        // traslazione di 8pixel per il print successivo 
-        offset1 += 8;
-        offset2 += 8;
-        // OUTPUT_5: └─────┘
-    }
 
+        //BottomCard - end
+        offset1 = 0;
+        offset2 = 6;
+        for(index = 0; index < nCard; index++){
+            // colore stampa
+            if(*(cardColore + index) == 'R'){
+                textcolor(12);
+            }else{
+                textcolor(15);
+            }
+            gotoxy(offset1, 5);
+            printf("%c", 192);
+            for(int j = 0; j < 5; j++)
+                printf("%c", 196);
+            gotoxy(offset2, 5);    
+            printf("%c", 217);
+            // traslazione di 8pixel per il print successivo 
+            offset1 += 8;
+            offset2 += 8;
+            // OUTPUT_5: └─────┘
+        }
+    }
     // FINAL RESULT
     // INDEX:      (0)     (1)     (2)     (3)
     // COLOR:      (R)     (R)    (B/W)   (B/W)
--- src/main.c
+++ src/main.c
@@ -26,10 +26,10 @@ struct deck_data{
 
 #include "tools.c"
 #include "startup.c"
-#include "deck.c"
 #include "graphics/sectorA.c"
 #include "graphics/sectorB.c"
 #include "graphics/sectorC.c"
+#include "deck.c"
 
 void textcolor(int colore);
 void gotoxy(short x, short y);
@@ -43,68 +43,75 @@ void sector_C(int nCard, int *cDorso, int *cValore, char *cSeme, char *cColore, 
 int main() {
     // console name
     SetConsoleTitle("Solitario Reglan Chiuso");
+    // Clear screen
     system("cls");
-    // splashscreen();
-
+    // Schemata iniziale
+    //splashscreen();
+    // Generazione del deck
     deckGeneration();
 
-    //
-    // START TEST 
-    //  
+    /* 
+        SECTOR_A - first rendering deck
+    */    
     
-    int cardN = 6, i;
     // Vettore dinamico
-    int *cardValore = NULL;
-    char *cardColore = NULL;
-    char *cardSeme = NULL;
+    int *sectorA_Valore = NULL;
+    char *sectorA_Colore = NULL;
+    char *sectorA_Seme = NULL;
     // puntatori - allocazione memoria
-    cardValore = (int*) calloc(cardN, sizeof(int));
-    cardColore = (char*) calloc(cardN, sizeof(char));
-    cardSeme = (char*) calloc(cardN, sizeof(char));
+    sectorA_Valore = (int*) calloc(6, sizeof(int));
+    sectorA_Colore = (char*) calloc(6, sizeof(char));
+    sectorA_Seme = (char*) calloc(6, sizeof(char));
 
-    // puntatori <--data-- vettori
-    for(i = 0; i < cardN; i++){
-        *(cardValore + i) = deck.valore[46 + i];
-        *(cardColore + i) = deck.colore[46 + i];
-        *(cardSeme + i) = deck.seme[46 + i];
+    // puntatori
+    for(int i = 0; i < 6; i++){
+        *(sectorA_Valore + i) = deck.valore[46 + i];
+        *(sectorA_Colore + i) = deck.colore[46 + i];
+        *(sectorA_Seme + i) = deck.seme[46 + i];
     }
+  
+    // visualizzazione carte
+    sector_A(6, sectorA_Valore, sectorA_Colore, sectorA_Seme);
 
+    /* 
+        SECTOR_B - first rendering deck
+    */ 
+
+    // Vettore dinamico
+    int *sectorB_Valore = NULL;
+    char *sectorB_Colore = NULL;
+    char *sectorB_Seme = NULL;
+    sectorB_Valore = (int*) calloc(4, sizeof(int));
+    sectorB_Colore = (char*) calloc(4, sizeof(char));
+    sectorB_Seme = (char*) calloc(4, sizeof(char));
     
-    // generazione delle carte
-    sector_A(cardN, cardValore, cardColore, cardSeme);
-    //free(cardValore);
-    //free(cardColore);
-    //free(cardSeme);
-
-    //
-    // END TEST
-    //
-
-    // puntatori <--data-- vettori
-    for(i = 0; i < 4; i++){
-        *(cardValore + i) = deck.valore[i];
-        *(cardColore + i) = deck.colore[i];
-        *(cardSeme + i) = deck.seme[i];
+    for(int i = 0; i < 4; i++){
+        *(sectorB_Valore + i) = deck.valore[i];
+        *(sectorB_Colore + i) = deck.colore[i];
+        *(sectorB_Seme + i) = deck.seme[i];
     }
 
-    sector_B(cardValore, cardColore, cardSeme);
+    sector_B(sectorB_Valore, sectorB_Colore, sectorB_Seme);
 
+    /* 
+        SECTOR_C - first rendering deck
+    */ 
    
     int nCard = 9;
     int offsetX = 0;
     // array_dataset
     int arr_dorso[9] = {1, 1, 1, 1, 1, 1, 1, 1, 0};
     
-    int *cDorso = NULL;
-    int *cValore = NULL;
-    char *cSeme = NULL;
-    char *cColore = NULL;
+    int *sectorC_Dorso = NULL;
+    int *sectorC_Valore = NULL;
+    char *sectorC_Seme = NULL;
+    char *sectorC_Colore = NULL;
 
     // puntatori - allocazione memoria
-    cDorso = (int*) calloc(nCard, sizeof(int));
-    cValore = (int*) calloc(nCard, sizeof(int));
-    cSeme = (char*) calloc(nCard, sizeof(char));
-    cColore = (char*) calloc(nCard, sizeof(char));
+    sectorC_Dorso = (int*) calloc(nCard, sizeof(int));
+    sectorC_Valore = (int*) calloc(nCard, sizeof(int));
+    sectorC_Seme = (char*) calloc(nCard, sizeof(char));
+    sectorC_Colore = (char*) calloc(nCard, sizeof(char));
 
     int getCard[9] = {7, 7, 7, 6, 5, 4, 3, 2, 1};
     int offsetData = 4;
@@ -112,28 +119,66 @@ int main() {
     for(int j = 0; j < 9; j++){
         // puntatori <--data-- vettori
         for(int i = 0; i < getCard[j]; i++){
-            *(cDorso + i) = arr_dorso[i];
-            *(cValore + i) = deck.valore[offsetData + i];
-            *(cSeme + i) = deck.seme[offsetData + i];
-            *(cColore + i) = deck.colore[offsetData + i];
+            *(sectorC_Dorso + i) = arr_dorso[i];
+            *(sectorC_Valore + i) = deck.valore[offsetData + i];
+            *(sectorC_Seme + i) = deck.seme[offsetData + i];
+            *(sectorC_Colore + i) = deck.colore[offsetData + i];
         }
 
         // int nCard, bool array_dorso[3], int array_valore[3], char array_seme[3], char array_colore[3], int offsetX
-        sector_C(getCard[j], cDorso, cValore, cSeme, cColore, offsetX);
+        sector_C(getCard[j], sectorC_Dorso, sectorC_Valore, sectorC_Seme, sectorC_Colore, offsetX);
         offsetData += getCard[j];
         offsetX += 8;
     }
 
-    //sector_C(5, 2, 8);
-    //sector_C(6, 3, 16);
-    //sector_C(numeroDorsi, numeroCard, 24);
-    //sector_C(numeroDorsi, numeroCard, 32);
-    //sector_C(numeroDorsi, numeroCard, 40);
-    //sector_C(numeroDorsi, numeroCard, 48);
-    //sector_C(numeroDorsi, numeroCard, 56);
-    //sector_C(numeroDorsi, numeroCard, 64);
+    //
+    // REALTIME UPDATE
+    // INSIDE GAME
+    //
+    int sector_A_nCard = 6;
+    while(1){
+        textcolor(15); 
+        gotoxy(0, 40);
+        printf("comando: ");
+        char c_1, c_2, c_3;
+        int from, to;
+        scanf("%c %c %c %i %i", &c_1, &c_2, &c_3, &from, &to);
 
-    textcolor(15);
-    getch(); 
+        // CHECK - comando
+        if(c_1 == 'A' && c_2 == '>' && c_3 == 'B'){
+            // TODO: CHECK FROM_VALUE && TO_VALUE
+            // CHECK - sequenza valori / seme delle carte
+            if(*(sectorA_Valore + from) == (*(sectorB_Valore + to) + 1) && *(sectorA_Seme + from) == *(sectorB_Seme + to)){
+                // SWAP VALORI
+                *(sectorB_Valore + to) = *(sectorA_Valore + from);
+                // Decremento il numero delle carte totali nel settore A
+                sector_A_nCard -= 1;
+                // SWAP POINTERS
+                for(int y = from; y < sector_A_nCard; y++){
+                    *(sectorA_Valore + y) = *(sectorA_Valore + (y + 1));
+                    *(sectorA_Colore + y) = *(sectorA_Colore + (y + 1));
+                    *(sectorA_Seme + y) = *(sectorA_Seme + (y + 1));
+                }                 
+                // refresh screen
+                system("cls");
+                sector_A(sector_A_nCard, sectorA_Valore, sectorA_Colore, sectorA_Seme);
+                sector_B(sectorB_Valore, sectorB_Colore, sectorB_Seme);
+                //sector_C(getCard[j], sectorC_Dorso, sectorC_Valore, sectorC_Seme, sectorC_Colore, offsetX);
+            }else{
+                printf("\n error\n");
+            }
+        }   
+    }
+
+    //TODO: FREE ALL POINTER
+    //free(sectorA_Valore);
+    //free(sectorA_Colore);
+    //free(sectorA_Seme);
+
+    // Fix text color
+    textcolor(15);  
+    // Clear screen
+    system("cls");
+    printf("Bye");
     return 0;
 }

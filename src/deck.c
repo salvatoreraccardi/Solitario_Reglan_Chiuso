7a64263df35fc2efea07894f84235693553bb1af
Salvatore Raccardi <36445663+salvatoreraccardi@users.noreply.github.com>
2020-05-25 20:31:27 +0200

    BUGFIX

--- src/deck.c
+++ src/deck.c
@@ -36,6 +36,7 @@ void deckGeneration(){
     // seed for random index
     srand(time(0)); 
     // Shuffling cards
+    
     for(swap = 0; swap < 300; swap++){
         rnd1 = 0;
         rnd2 = 0;
